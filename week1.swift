e2de32018d960d7c9599d6a1ac15304f2786cfcb
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-09-15 16:28:34 +0300

    Update week1.swift

--- week1.swift
+++ week1.swift
@@ -13,3 +13,29 @@ case.tea:
 case.water:
     print("water is my fav ")
 }
+
+
+//add additional info to tirmaisu case
+
+enum Dessert {
+    case tirmaisu(chefName: String?) //next the enum case i specify the type of value
+    //add chefname as arg label for value
+    case tea
+    case water
+}
+
+let fav = Dessert.tirmaisu(chefName: "maikle")//add chefname
+switch fav {
+case.tirmaisu(let chefName)://updated
+    let prefix: String
+    if let chefName = chefName{
+        prefix = "\(chefName)"}
+    else {
+        prefix = ""
+    }
+    print("\(prefix)tir is my fav")
+case.tea:
+    print("tea is my fav")
+case.water:
+    print("water is my fav ")
+}
