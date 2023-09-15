a404c15f597d4471d64a2c4cc5fea68a2349be3b
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-09-15 16:19:23 +0300

    Create week1.swift

---
+++ week1.swift
@@ -0,0 +1,15 @@
+enum Desser {
+    case tirmaisu
+    case tea
+    case water
+}
+
+let fav = Desser.tirmaisu
+switch fav {
+case.tirmaisu:
+    print("tir is my fav")
+case.tea:
+    print("tea is my fav")
+case.water:
+    print("water is my fav ")
+}
